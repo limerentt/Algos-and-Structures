2f72cbb703fd6c0bffe16e7fee219ef7bcb582d6
limerentt <94709578+limerentt@users.noreply.github.com>
2023-02-12 17:12:51 +0300

    Update fft.cpp

--- Number_theory/fft.cpp
+++ Number_theory/fft.cpp
@@ -1,48 +1,68 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
+#include <complex>
+#include <iostream>
+#include <numbers>
+#include <vector>
 
-void FFT(vector<complex<double>>& p, bool inv) {
-    ll deg = p.size();
-    if (deg == 1) return;
-    vector<complex<double>> p0(deg / 2), p1(deg / 2);
-    for (ll i = 0; i < deg / 2; ++i)
-        p0[i] = p[2 * i], p1[i] = p[2 * i + 1];
+static const double kPi = 3.14159265358979323846;
 
-    FFT(p0, inv), FFT(p1, inv);
+void fft(std::vector<std::complex<double>>& p, bool inv) {
+  int deg = int(p.size());
+  if (deg == 1) {
+    return;
+  }
 
-    double angle = (2.0 * numbers::pi) / double(deg);
-    if (inv) angle *= -1;
-    complex<double> prim_w = polar(double(1), angle);
-    complex<double> w_n = 1;
+  std::vector<std::complex<double>> p0(deg / 2);
+  std::vector<std::complex<double>> p1(deg / 2);
+  for (int i = 0; i < deg / 2; ++i) {
+    p0[i] = p[2 * i], p1[i] = p[2 * i + 1];
+  }
 
-    for (ll i = 0; i < deg / 2; ++i, w_n *= prim_w) {
-        p[i] = p0[i] + w_n * p1[i];
-        p[i + deg / 2] = p0[i] - w_n * p1[i];
-    }
+  fft(p0, inv), fft(p1, inv);
+
+  double angle = (2 * kPi) / double(deg);
+  if (inv) {
+    angle *= -1;
+  }
+  std::complex<double> prim_w = std::polar(double(1), angle);
+  std::complex<double> w_n = 1;
+
+  for (int i = 0; i < deg / 2; ++i, w_n *= prim_w) {
+    p[i] = p0[i] + w_n * p1[i];
+    p[i + deg / 2] = p0[i] - w_n * p1[i];
+  }
 }
 
-signed main() {
-    ll n, m;
-    cin >> n;
-    vector<complex<double>> a(n + 1);
-    for (ll i = n; i >= 0; --i) cin >> a[i];
-    cin >> m;
-    vector<complex<double>> b(m + 1);
-    for (ll i = m; i >= 0; --i) cin >> b[i];
+int main() {
+  int n;
+  int m;
+  std::cin >> n;
+  std::vector<std::complex<double>> a(n + 1);
+  for (int i = n; i >= 0; --i) {
+    std::cin >> a[i];
+  }
+  std::cin >> m;
+  std::vector<std::complex<double>> b(m + 1);
+  for (int i = m; i >= 0; --i) {
+    std::cin >> b[i];
+  }
 
-    ll k = n + m;
-    while (k & (k + 1)) ++k; //k + 1 is a pow of 2
+  int k = n + m;
+  while ((k & (k + 1)) != 0) {
+    ++k;
+  }
 
-    a.resize(k + 1, 0);
-    b.resize(k + 1, 0);
+  a.resize(k + 1, 0);
+  b.resize(k + 1, 0);
+  fft(a, false), fft(b, false);
 
-    FFT(a, false), FFT(b, false);
+  std::vector<std::complex<double>> c(k + 1, 0);
+  for (int i = 0; i <= k; ++i) {
+    c[i] = a[i] * b[i];
+  }
 
-    vector<complex<double>> c(k + 1, 0);
-    for (ll i = 0; i <= k; ++i) c[i] = a[i] * b[i];
-
-    FFT(c, true);
-    cout << n + m << ' ';
-    for (ll i = n + m; i >= 0; --i) cout << lround((c[i] / complex<double>(k + 1)).real()) << ' ';
+  fft(c, true);
+  std::cout << n + m << ' ';
+  for (int i = n + m; i >= 0; --i) {
+    std::cout << lround((c[i] / std::complex<double>(k + 1)).real()) << ' ';
+  }
 }
