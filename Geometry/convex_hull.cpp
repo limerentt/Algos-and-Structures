e9088e3bfb4776d7536adf67c211b16f8fc7be36
limerentt <94709578+limerentt@users.noreply.github.com>
2023-02-12 17:09:49 +0300

    Update convex_hull.cpp

--- Geometry/convex_hull.cpp
+++ Geometry/convex_hull.cpp
@@ -1,95 +1,115 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-using ld = long double;
-static const ld eps = 1e-12;
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <vector>
 
+static const int kOuputPrecision = 10, kPrebackNum = 3;
 
-class v {
+class Point {
 public:
-    ll x, y;
+  int x, y;
 
-    v() = default;
-    v(ll xx, ll yy): x(xx), y(yy) {}
-    v(ll lx, ll ly, ll rx, ll ry) {
-        x = rx - lx;
-        y = ry - ly;
-    }
-    ll len() { return sqrt(x * x + y * y); }
-    ll vec_prod(v a) { return x * a.y - y * a.x; }
-    ll dot_prod(v a) { return x * a.x + y * a.y; }
+  Point() = default;
+  Point(int xx, int yy) : x(xx), y(yy) {}
+  Point(int lx, int ly, int rx, int ry) {
+    x = rx - lx;
+    y = ry - ly;
+  }
+  int len() const {
+    return sqrt(x * x + y * y);
+  }
+  int vecProd(Point a) const {
+    return x * a.y - y * a.x;
+  }
+  int dotProd(Point a) const {
+    return x * a.x + y * a.y;
+  }
 };
 
-v base;
+Point base;
 
-bool cmp_ang(v a, v b) {
-    v an(a.x - base.x, a.y - base.y);
-    v bn(b.x - base.x, b.y - base.y);
-    ll vp = an.vec_prod(bn);
-    return vp > 0 || vp == 0 && an.x * an.x + an.y * an.y < bn.x * bn.x + bn.y * bn.y;
+bool cmpAng(Point a, Point b) {
+  Point an(a.x - base.x, a.y - base.y);
+  Point bn(b.x - base.x, b.y - base.y);
+  int vp = an.vecProd(bn);
+  return vp > 0 ||
+         (vp == 0 && an.x * an.x + an.y * an.y < bn.x * bn.x + bn.y * bn.y);
+}
+
+std::vector<Point> buildHull(int n, const std::vector<Point>& nice_polygon) {
+  std::vector<Point> hull;
+  hull.push_back(base);
+
+  for (int i = 0; i < n - 1; ++i) {
+    hull.push_back(nice_polygon[i]);
+
+    int vp = 1;
+    while (vp >= 0 && int(hull.size()) >= kPrebackNum) {
+      Point vertex = hull[hull.size() - 1];
+      Point back = hull[hull.size() - 2];
+      Point preback = hull[hull.size() - kPrebackNum];
+
+      Point first_segm(back.x, back.y, preback.x, preback.y);
+      Point second_segm(back.x, back.y, vertex.x, vertex.y);
+
+      vp = first_segm.vecProd(second_segm);
+
+      if (vp >= 0) {
+        hull.pop_back();
+        hull.pop_back();
+        hull.push_back(vertex);
+      }
+    }
+  }
+
+  return hull;
+}
+
+std::vector<Point> normalizePolygon(int n, std::vector<Point> polygon) {
+  base = polygon[0];
+  int index = 0;
+  for (int i = 0; i < n; ++i) {
+    if (base.y > polygon[i].y ||
+        (polygon[i].y == base.y && polygon[i].x < base.x)) {
+      base = polygon[i];
+      index = i;
+    }
+  }
+
+  std::vector<Point> nice_polygon(n - 1);
+  for (int i = 0, j = 0; j < n; ++i, ++j) {
+    if (i == index) {
+      if (j + 1 < n) {
+        ++j;
+      } else {
+        break;
+      }
+    }
+    nice_polygon[i] = polygon[j];
+  }
+
+  std::sort(nice_polygon.begin(), nice_polygon.end(), &cmpAng);
+
+  return nice_polygon;
 }
 
 int main() {
-    cout << setprecision(10);
+  std::cout << std::setprecision(kOuputPrecision);
 
-    ll n;
-    cin >> n;
-    vector<v> p(n);
+  int n;
+  std::cin >> n;
 
-    for (ll i = 0; i < n; ++i) {
-        pair<ll, ll> elem;
-        cin >> elem.first >> elem.second;
-        v to_make(elem.first, elem.second);
-        p[i] = to_make;
-    }
+  std::vector<Point> polygon(n);
+  for (int i = 0; i < n; ++i) {
+    std::cin >> polygon[i].x >> polygon[i].y;
+  }
 
-    base = p[0];
-    ll index = 0;
-    for (ll i = 0; i < n; ++i) {
-        if (base.y > p[i].y || p[i].y == base.y && p[i].x < base.x) {
-            base = p[i];
-            index = i;
-        }
-    }
+  polygon = normalizePolygon(n, polygon);
 
-
-    vector<v> pp(n - 1);
-    for (ll i = 0, j = 0; j < n; ++i, ++j) {
-        if (i == index) {
-            if (j + 1 < n) ++j;
-            else break;
-        }
-        pp[i] = p[j];
-    }
-
-    sort(pp.begin(), pp.end(), &cmp_ang);
-
-    vector<v> hull;
-    hull.push_back(base);
-
-    for (ll i = 0; i < n - 1; ++i) {
-        hull.push_back(pp[i]);
-
-        ll vp = 1;
-        while (vp >= 0 && hull.size() >= 3) {
-            v vertex = hull[hull.size() - 1];
-            v back = hull[hull.size() - 2];
-            v preback = hull[hull.size() - 3];
-
-            v first_segm(back.x, back.y, preback.x, preback.y);
-            v second_segm(back.x, back.y, vertex.x, vertex.y);
-
-            vp = first_segm.vec_prod(second_segm);
-
-            if (vp >= 0) {
-                hull.pop_back();
-                hull.pop_back();
-                hull.push_back(vertex);
-            }
-        }
-    }
-
-    cout << hull.size() << '\n';
-
-    for (auto point: hull) cout << point.x << ' ' << point.y << '\n';
+  std::vector<Point> hull = buildHull(n, polygon);
+  std::cout << hull.size() << '\n';
+  for (auto point : hull) {
+    std::cout << point.x << ' ' << point.y << '\n';
+  }
 }
