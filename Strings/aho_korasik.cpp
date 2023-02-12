3d394c720e36797c59a7fc63498643a57d90841c
limerentt <94709578+limerentt@users.noreply.github.com>
2023-02-12 16:18:51 +0300

    Update aho_korasik.cpp

--- Strings/aho_korasik.cpp
+++ Strings/aho_korasik.cpp
@@ -1,83 +1,118 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
+#include <iostream>
+#include <queue>
+#include <vector>
 
-struct node {
-    node() { to.assign(26, -1); }
-    vector<int> to;
-    vector<int> indices;
-    ll depth = 0;
-    int link = -1;
+static const int kAlphabetSize = 26;
+static const char kStartAlphabet = 'a';
+static const char kEndAlphabet = 'z';
+
+struct Node {
+  Node() {
+    to.assign(kAlphabetSize, -1);
+  }
+  std::vector<int> to;
+  std::vector<int> indices;
+  int depth = 0;
+  int link = -1;
 };
 
-void add(vector<node>& trie, const string& s, int index) {
-    int v = 0;
-    for (auto x: s) {
-        if (trie[v].to[x - 'a'] == -1) {
-            trie.push_back(node());
-            trie[v].to[x - 'a'] = int(trie.size()) - 1;
-        }
-        v = trie[v].to[x - 'a'];
-    }
-    trie[v].depth = s.size();
-    trie[v].indices.push_back(index);
-}
+class Trie {
+public:
+  std::vector<Node> trie;
+  Trie() {
+    trie.push_back(Node());
+  }
 
-void koras(vector<node>& trie) {
-    trie[0].link = 0;
-    for (char i = 'a'; i <= 'z'; ++i) {
-        if (trie[0].to[i - 'a'] != -1) continue;
-        else trie[0].to[i - 'a'] = 0;
+  void add(const std::string& proceeding_string, int index) {
+    int v = 0;
+    for (auto ch : proceeding_string) {
+      if (trie[v].to[ch - kStartAlphabet] == -1) {
+        trie.push_back(Node());
+        trie[v].to[ch - kStartAlphabet] = int(trie.size()) - 1;
+      }
+      v = trie[v].to[ch - kStartAlphabet];
     }
-    queue<int> q;
+    trie[v].depth = proceeding_string.size();
+    trie[v].indices.push_back(index);
+  }
+
+  void ahoKorasik() {
+    trie[0].link = 0;
+    for (char i = kStartAlphabet; i <= kEndAlphabet; ++i) {
+      if (trie[0].to[i - kStartAlphabet] == -1) {
+        trie[0].to[i - kStartAlphabet] = 0;
+      }
+    }
+    std::queue<int> q;
     q.push(0);
     while (!q.empty()) {
-        int v = q.front();
-        q.pop();
-        for (int i = 0; i < trie[v].to.size(); ++i) {
-            int u = trie[v].to[i];
-            if (trie[u].link != -1) continue;
-            if (v == 0) trie[u].link = 0;
-            else trie[u].link = trie[trie[v].link].to[i];
-            for (char c = 'a'; c <= 'z'; ++c) {
-                if (trie[u].to[c - 'a'] != -1) continue;
-                trie[u].to[c - 'a'] = trie[trie[u].link].to[c - 'a'];
-            }
-            q.push(u);
+      int v = q.front();
+      q.pop();
+      for (int i = 0; i < int(trie[v].to.size()); ++i) {
+        int son = trie[v].to[i];
+        if (trie[son].link != -1) {
+          continue;
         }
+        if (v == 0) {
+          trie[son].link = 0;
+        } else {
+          trie[son].link = trie[trie[v].link].to[i];
+        }
+        for (char c = kStartAlphabet; c <= kEndAlphabet; ++c) {
+          if (trie[son].to[c - kStartAlphabet] != -1) {
+            continue;
+          }
+          trie[son].to[c - kStartAlphabet] =
+            trie[trie[son].link].to[c - kStartAlphabet];
+        }
+        q.push(son);
+      }
     }
-}
+  }
+
+  std::vector<std::vector<int>> countEntrance(int n,
+                                              std::string initial_string) {
+    std::vector<std::vector<int>> entrances(n);
+    int v = 0;
+    int u;
+    for (int i = 0; i < int(initial_string.size()); ++i) {
+      v = trie[v].to[initial_string[i] - kStartAlphabet];
+      u = v;
+      while (u != 0) {
+        for (auto elem : trie[u].indices) {
+          entrances[elem].push_back(i - trie[u].depth + 2);
+        }
+        u = trie[u].link;
+      }
+    }
+
+    return entrances;
+  }
+};
 
 int main() {
-    string start, str;
-    int n;
+  std::string initial_string;
+  int n;
+  std::cin >> initial_string >> n;
 
-    cin >> start >> n;
+  Trie my_trie;
 
-    vector<node> trie;
-    trie.push_back(node());
+  std::string word;
+  for (int i = 0; i < n; ++i) {
+    std::cin >> word;
+    my_trie.add(word, i);
+  }
 
-    for (int i = 0; i < n; ++i) {
-        cin >> str;
-        add(trie, str, i);
+  my_trie.ahoKorasik();
+
+  std::vector<std::vector<int>> entrances =
+    my_trie.countEntrance(n, initial_string);
+
+  for (int i = 0; i < n; ++i) {
+    std::cout << entrances[i].size() << ' ';
+    for (const auto& index : entrances[i]) {
+      std::cout << index << ' ';
     }
-
-    koras(trie);
-
-    int v = 0, u;
-    vector<vector<int>> ans(n);
-    for (int i = 0; i < start.size(); ++i) {
-        v = trie[v].to[start[i] - 'a'];
-        u = v;
-        while (u != 0) {
-            for (auto elem: trie[u].indices) ans[elem].push_back(i - trie[u].depth + 2);
-            u = trie[u].link;
-        }
-    }
-
-    for (int i = 0; i < n; ++i) {
-        cout << ans[i].size() << ' ';
-        for (const auto& elem: ans[i]) cout << elem << ' ';
-        cout << '\n';
-    }
+    std::cout << '\n';
+  }
 }
